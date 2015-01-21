c67facb6b85b86e2703f124138714aefbf420351
Sandi Metz <sandi@sandimetz.com>
2015-01-21 06:33:24 -0500

    slight house refactor to give them a fighting chance

--- house/lib/house.rb
+++ house/lib/house.rb
@@ -1,5 +1,25 @@
 class House
 
+  DATA = [
+    "the horse and the hound and the horn that belonged to",
+    "the farmer sowing his corn that kept",
+    "the rooster that crowed in the morn that woke",
+    "the priest all shaven and shorn that married",
+    "the man all tattered and torn that kissed",
+    "the maiden all forlorn that milked",
+    "the cow with the crumpled horn that tossed",
+    "the dog that worried",
+    "the cat that killed",
+    "the rat that ate",
+    "the malt that lay in",
+    "the house that Jack built"]
+
+  attr_reader :data
+
+  def initialize
+    @data = DATA
+  end
+
   def recite
     1.upto(12).collect {|i| line(i)}.join("\n")
   end
@@ -9,17 +29,9 @@ class House
   end
 
   def phrase(num)
-    [ "the horse and the hound and the horn that belonged to",
-      "the farmer sowing his corn that kept",
-      "the rooster that crowed in the morn that woke",
-      "the priest all shaven and shorn that married",
-      "the man all tattered and torn that kissed",
-      "the maiden all forlorn that milked",
-      "the cow with the crumpled horn that tossed",
-      "the dog that worried",
-      "the cat that killed",
-      "the rat that ate",
-      "the malt that lay in",
-      "the house that Jack built"].last(num).join(" ")
+    data.last(num).join(" ")
   end
 end
+
+puts
+puts House.new.line(12)
